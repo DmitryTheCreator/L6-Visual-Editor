8dfb89335b291896e1235121e1c5be30e174b6a9
DmitryTheCreator <dmitriyzamorev@gmail.com>
2020-12-11 00:37:56 +0500

    Version 2
    Реализовал выделение объектов

--- L6 Visual Editor/Form1.Designer.cs
+++ L6 Visual Editor/Form1.Designer.cs
@@ -28,100 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel = new System.Windows.Forms.Panel();
-            this.btnSquare = new System.Windows.Forms.Button();
-            this.btnTriangle = new System.Windows.Forms.Button();
-            this.btnCircle = new System.Windows.Forms.Button();
             this.lblColor = new System.Windows.Forms.Label();
             this.pbColor = new System.Windows.Forms.PictureBox();
             this.pbBorder = new System.Windows.Forms.PictureBox();
             this.lblBorder = new System.Windows.Forms.Label();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.cmbxShape = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pbColor)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbBorder)).BeginInit();
             this.SuspendLayout();
             // 
             // panel
             // 
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel.BackColor = System.Drawing.Color.White;
             this.panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel.Location = new System.Drawing.Point(245, 0);
+            this.panel.Location = new System.Drawing.Point(12, 49);
             this.panel.Name = "panel";
-            this.panel.Size = new System.Drawing.Size(869, 596);
+            this.panel.Size = new System.Drawing.Size(1090, 535);
             this.panel.TabIndex = 0;
             this.panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
-            // 
-            // btnSquare
-            // 
-            this.btnSquare.BackColor = System.Drawing.Color.PeachPuff;
-            this.btnSquare.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnSquare.FlatAppearance.BorderSize = 0;
-            this.btnSquare.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSquare.Image = ((System.Drawing.Image)(resources.GetObject("btnSquare.Image")));
-            this.btnSquare.Location = new System.Drawing.Point(6, 49);
-            this.btnSquare.Name = "btnSquare";
-            this.btnSquare.Size = new System.Drawing.Size(66, 66);
-            this.btnSquare.TabIndex = 1;
-            this.btnSquare.UseVisualStyleBackColor = false;
-            this.btnSquare.Click += new System.EventHandler(this.btnSquare_Click);
-            // 
-            // btnTriangle
-            // 
-            this.btnTriangle.BackColor = System.Drawing.Color.PeachPuff;
-            this.btnTriangle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnTriangle.FlatAppearance.BorderSize = 0;
-            this.btnTriangle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnTriangle.Image = ((System.Drawing.Image)(resources.GetObject("btnTriangle.Image")));
-            this.btnTriangle.Location = new System.Drawing.Point(87, 49);
-            this.btnTriangle.Name = "btnTriangle";
-            this.btnTriangle.Size = new System.Drawing.Size(66, 66);
-            this.btnTriangle.TabIndex = 2;
-            this.btnTriangle.UseVisualStyleBackColor = false;
-            this.btnTriangle.Click += new System.EventHandler(this.btnTriangle_Click);
-            // 
-            // btnCircle
-            // 
-            this.btnCircle.BackColor = System.Drawing.Color.PeachPuff;
-            this.btnCircle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnCircle.FlatAppearance.BorderSize = 0;
-            this.btnCircle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCircle.Image = ((System.Drawing.Image)(resources.GetObject("btnCircle.Image")));
-            this.btnCircle.Location = new System.Drawing.Point(167, 49);
-            this.btnCircle.Name = "btnCircle";
-            this.btnCircle.Size = new System.Drawing.Size(66, 66);
-            this.btnCircle.TabIndex = 3;
-            this.btnCircle.UseVisualStyleBackColor = false;
-            this.btnCircle.Click += new System.EventHandler(this.btnCircle_Click);
             // 
             // lblColor
             // 
             this.lblColor.AutoSize = true;
-            this.lblColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblColor.Location = new System.Drawing.Point(12, 148);
+            this.lblColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblColor.ForeColor = System.Drawing.Color.Black;
+            this.lblColor.Location = new System.Drawing.Point(198, 11);
             this.lblColor.Name = "lblColor";
-            this.lblColor.Size = new System.Drawing.Size(167, 32);
+            this.lblColor.Size = new System.Drawing.Size(136, 25);
             this.lblColor.TabIndex = 4;
-            this.lblColor.Text = "Цвет фигур";
+            this.lblColor.Text = "Цвет фигуры";
             // 
             // pbColor
             // 
             this.pbColor.BackColor = System.Drawing.Color.Salmon;
-            this.pbColor.Location = new System.Drawing.Point(185, 148);
+            this.pbColor.Location = new System.Drawing.Point(340, 13);
             this.pbColor.Name = "pbColor";
-            this.pbColor.Size = new System.Drawing.Size(36, 32);
+            this.pbColor.Size = new System.Drawing.Size(30, 30);
             this.pbColor.TabIndex = 5;
             this.pbColor.TabStop = false;
             this.pbColor.Click += new System.EventHandler(this.pbColor_Click);
             // 
             // pbBorder
             // 
-            this.pbBorder.BackColor = System.Drawing.Color.White;
-            this.pbBorder.Location = new System.Drawing.Point(185, 202);
+            this.pbBorder.BackColor = System.Drawing.Color.Black;
+            this.pbBorder.Location = new System.Drawing.Point(522, 13);
             this.pbBorder.Name = "pbBorder";
-            this.pbBorder.Size = new System.Drawing.Size(36, 32);
+            this.pbBorder.Size = new System.Drawing.Size(30, 30);
             this.pbBorder.TabIndex = 7;
             this.pbBorder.TabStop = false;
             this.pbBorder.Click += new System.EventHandler(this.pbBorder_Click);
@@ -129,26 +87,40 @@
             // lblBorder
             // 
             this.lblBorder.AutoSize = true;
-            this.lblBorder.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblBorder.Location = new System.Drawing.Point(12, 202);
+            this.lblBorder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblBorder.Location = new System.Drawing.Point(393, 11);
             this.lblBorder.Name = "lblBorder";
-            this.lblBorder.Size = new System.Drawing.Size(169, 32);
+            this.lblBorder.Size = new System.Drawing.Size(123, 25);
             this.lblBorder.TabIndex = 6;
             this.lblBorder.Text = "Цвет рамки";
+            // 
+            // cmbxShape
+            // 
+            this.cmbxShape.DropDownWidth = 108;
+            this.cmbxShape.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cmbxShape.FormattingEnabled = true;
+            this.cmbxShape.Items.AddRange(new object[] {
+            "Квадрат",
+            "Треугольник",
+            "Круг"});
+            this.cmbxShape.Location = new System.Drawing.Point(12, 8);
+            this.cmbxShape.Name = "cmbxShape";
+            this.cmbxShape.Size = new System.Drawing.Size(160, 33);
+            this.cmbxShape.TabIndex = 8;
+            this.cmbxShape.Text = "Фигура";
+            this.cmbxShape.SelectedIndexChanged += new System.EventHandler(this.cmbxShape_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(1114, 596);
+            this.Controls.Add(this.cmbxShape);
             this.Controls.Add(this.pbBorder);
             this.Controls.Add(this.lblBorder);
             this.Controls.Add(this.pbColor);
             this.Controls.Add(this.lblColor);
-            this.Controls.Add(this.btnCircle);
-            this.Controls.Add(this.btnTriangle);
-            this.Controls.Add(this.btnSquare);
             this.Controls.Add(this.panel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
@@ -163,15 +135,13 @@
         #endregion
 
         private System.Windows.Forms.Panel panel;
-        private System.Windows.Forms.Button btnSquare;
-        private System.Windows.Forms.Button btnTriangle;
-        private System.Windows.Forms.Button btnCircle;
         private System.Windows.Forms.Label lblColor;
         private System.Windows.Forms.PictureBox pbBorder;
         private System.Windows.Forms.Label lblBorder;
         private System.Windows.Forms.ColorDialog colorDialog1;
         private System.Windows.Forms.ColorDialog colorDialog2;
         private System.Windows.Forms.PictureBox pbColor;
+        private System.Windows.Forms.ComboBox cmbxShape;
     }
 }
 
--- L6 Visual Editor/Form1.cs
+++ L6 Visual Editor/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace L6_Visual_Editor
@@ -15,7 +11,7 @@ namespace L6_Visual_Editor
     {
 
         Storage storage;
-
+        
         public Form1()
         {
             InitializeComponent();
@@ -25,39 +21,57 @@ namespace L6_Visual_Editor
 
         public void UpdateFormModel(object sender, EventArgs e)
         {
-            foreach (Figure figure in storage)
-            {
-
-            }
+            draw();
         }
 
         public void draw()
         {
-            SolidBrush br = new SolidBrush(Color.Red);
-            foreach (Figure figure in storage)
+            if (storage.first() == null)
+                return;
+            var it = storage.first();          
+            while (it != null)
             {
-                if (storage.Status == "Square")
+                SolidBrush cr = new SolidBrush(it.Color);
+                Pen br = new Pen(it.Border, it.BorderSize);
+                if (it.BorderChange == false)
+                    br = new Pen(Color.White, it.BorderSize);
+
+                if (it.Shape == "Square")
                 {
-                    panel.CreateGraphics().FillRectangle(br, new Rectangle(figure.X - figure.Radius,
-                        figure.Y - figure.Radius, 2 * figure.Radius, 2 * figure.Radius));
+                    panel.CreateGraphics().FillRectangle(cr, new Rectangle(it.X - it.Radius,
+                        it.Y - it.Radius, 2 * it.Radius, 2 * it.Radius));
+
+                    if (it.BorderChange == true)
+                    {
+                        panel.CreateGraphics().DrawRectangle(br, new Rectangle(it.X - it.Radius - it.BorderSize / 2,
+                            it.Y - it.Radius - it.BorderSize / 2, 2 * it.Radius + it.BorderSize / 2, 2 * it.Radius + it.BorderSize / 2));
+                        it.BorderChange = false;
+                    }
                 }
-                if (storage.Status == "Circle")
-                {
-                    panel.CreateGraphics().FillEllipse(br, figure.X - figure.Radius,
-                       figure.Y - figure.Radius, 2 * figure.Radius, 2 * figure.Radius);
+                if (it.Shape == "Circle")
+                {                 
+                    panel.CreateGraphics().FillEllipse(cr, it.X - it.Radius,
+                       it.Y - it.Radius, 2 * it.Radius, 2 * it.Radius);
+
+                    if (it.BorderChange == true)
+                        panel.CreateGraphics().DrawEllipse(br, it.X - it.Radius - it.BorderSize / 2,
+                            it.Y - it.Radius - it.BorderSize / 2, 2 * it.Radius + it.BorderSize / 2, 2 * it.Radius + it.BorderSize / 2);
                 }
 
-                if (storage.Status == "Triangle")
+                if (it.Shape == "Triangle")
                 {
+                    int border = 0;
+                    if (it.BorderChange == true) border = it.BorderSize;
+
                     List<Point> line1 = new List<Point>();
                     List<Point> line2 = new List<Point>();
                     List<Point> line3 = new List<Point>();
-                    line1.Add(new Point(figure.X, figure.Y - figure.Radius));
-                    line1.Add(new Point(figure.X + figure.Radius, figure.Y + figure.Radius / 2));
-                    line2.Add(new Point(figure.X, figure.Y - figure.Radius));
-                    line2.Add(new Point(figure.X - figure.Radius, figure.Y + figure.Radius / 2));
-                    line3.Add(new Point(figure.X - figure.Radius, figure.Y + figure.Radius / 2));
-                    line3.Add(new Point(figure.X + figure.Radius, figure.Y + figure.Radius / 2));
+                    line1.Add(new Point(it.X, it.Y - it.Radius - border));
+                    line1.Add(new Point(it.X + it.Radius + border, it.Y + it.Radius / 2 + border / 2));
+                    line2.Add(new Point(it.X, it.Y - it.Radius - border));
+                    line2.Add(new Point(it.X - it.Radius - border, it.Y + it.Radius / 2 + border / 2));
+                    line3.Add(new Point(it.X - it.Radius - border, it.Y + it.Radius / 2 + border / 2));
+                    line3.Add(new Point(it.X + it.Radius + border, it.Y + it.Radius / 2 + border / 2));
 
                     GraphicsPath myPath = new GraphicsPath();
                     myPath.StartFigure();
@@ -66,8 +80,13 @@ namespace L6_Visual_Editor
                     myPath.AddLines(line3.ToArray());
                     myPath.CloseFigure();
 
-                    panel.CreateGraphics().FillPath(br, myPath);
-                }
+                    panel.CreateGraphics().FillPath(cr, myPath);
+
+                    if (it.BorderChange == true)
+                        panel.CreateGraphics().DrawPath(br, myPath);
+                    
+                }                
+                it = storage.next(it);
             }
         }
 
@@ -81,38 +100,30 @@ namespace L6_Visual_Editor
                         Figure figure = new Figure();
                         figure.X = e.X;
                         figure.Y = e.Y;
-                        figure.Radius = 30;
                         figure.Shape = storage.Status;
+                        figure.Color = pbColor.BackColor;
+                        figure.Border = pbBorder.BackColor;
 
                         if (storage.is_inside(e.X, e.Y) != null)
                         {
+                            //if (storage.is_inside(e.X, e.Y).BorderChange == false)
+                            storage.is_inside(e.X, e.Y).BorderChange = true;
+                            //else storage.is_inside(e.X, e.Y).BorderChange = false;
                             storage.is_inside(e.X, e.Y).Border = pbBorder.BackColor;
+                            storage.observers.Invoke(this, null);
                         }
-                        else storage.add(figure);
-                        draw();
+                        else storage.add(figure);                    
                         break;
                     }
             }
         }
 
-        private void btnSquare_Click(object sender, EventArgs e)
-        {
-            storage.Status = "Square";
-        }
-
-        private void btnTriangle_Click(object sender, EventArgs e)
-        {
-            storage.Status = "Triangle";
-        }
-
-        private void btnCircle_Click(object sender, EventArgs e)
-        {
-            storage.Status = "Circle";
-        }
+ 
 
         public class Storage
         {
             private LinkedList<Figure> storage = new LinkedList<Figure>();
+
             private string status;
             public string Status { set { status = value; } get { return status; } }
 
@@ -120,6 +131,25 @@ namespace L6_Visual_Editor
             {
                 storage.AddLast(figure);
                 observers.Invoke(this, null);
+            }
+
+            public Figure first()
+            { 
+                if (storage.Count != 0) 
+                    return storage.First();
+                return null;
+            }
+            
+            public Figure next(Figure figure) 
+            {
+                bool check = false;
+                foreach(Figure fig in storage)
+                {
+                    if (check == true) return fig;
+                    if (figure == fig)
+                        check = true;
+                }
+                return null; 
             }
 
             public Figure is_inside(int x0, int y0)
@@ -134,9 +164,8 @@ namespace L6_Visual_Editor
                                 int y1 = figure.Y - figure.Radius;
                                 int x3 = figure.X + figure.Radius;
                                 int y3 = figure.Y + figure.Radius;
-                                if (x0 > x1 && y0 > y1 && x0 > x3 && y0 > y3)
-                                {
-                   
+                                if (x0 > x1 && y0 > y1 && x0 < x3 && y0 < y3)
+                                {        
                                     observers.Invoke(this, null);
                                     return figure;
                                 }
@@ -144,18 +173,16 @@ namespace L6_Visual_Editor
                             }
                         case "Triangle":
                             {
-                                int x1 = figure.X - figure.Radius;
-                                int y1 = figure.Y + figure.Radius / 2;
-                                int x2 = figure.X;
-                                int y2 = figure.Radius;
-                                int x3 = figure.X + figure.Radius;
+                                int x1 = figure.X;
+                                int y1 = figure.Y - figure.Radius;
+                                int x2 = figure.X + figure.Radius;
+                                int y2 = figure.Y + figure.Radius / 2;
+                                int x3 = figure.X - figure.Radius;
                                 int y3 = figure.Y + figure.Radius / 2;
-                                float alpha = ((y2 - y3) * (x0 - x3) + (x3 - x2) * (y0 - y3)) /
-                                    ((y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3));
-                                float beta = ((y3 - y1) * (x0 - x3) + (x1 - x3) * (y0 - y3)) /
-                                    ((y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3));
-                                float gamma = 1.0f - alpha - beta;
-                                if (alpha > 0 && beta > 0 && gamma > 0)
+                                int alpha = (x1 - x0) * (y2 - y1) - (x2 - x1) * (y1 - y0);
+                                int beta = (x2 - x0) * (y3 - y2) - (x3 - x2) * (y2 - y0);
+                                int gamma = (x3 - x0) * (y1 - y3) - (x1 - x3) * (y3 - y0);
+                                if (((alpha > 0) && (beta > 0) && (gamma > 0)) || ((alpha < 0) && (beta < 0) && (gamma < 0)))
                                 {
                                     observers.Invoke(this, null);
                                     return figure;
@@ -167,18 +194,26 @@ namespace L6_Visual_Editor
                                 int sum = Convert.ToInt32(Math.Pow(x0 - figure.X, 2) + Math.Pow(y0 - figure.Y, 2));
                                 int rad = Convert.ToInt32(Math.Pow(figure.Radius, 2));
                                 if (sum <= rad)
-                                {
+                                {                                 
                                     observers.Invoke(this, null);
                                     return figure;
                                 }
                                 break;
                             }
                     }
+                    
                 }
                 return null;
             }
 
-
+            public void change_color(Color color)
+            {
+                foreach(Figure figure in storage)
+                {
+                    figure.Color = color;
+                }
+                observers.Invoke(this, null);
+            }
 
             public EventHandler observers;
         }
@@ -191,7 +226,7 @@ namespace L6_Visual_Editor
             public int X { set { x = value; } get { return x; } }
             public int Y { set { y = value; } get { return y; } }
 
-            private int radius;
+            protected int radius = 30;
             public int Radius { set { radius = value; } get { return radius; } }
 
             protected Color color;
@@ -200,7 +235,13 @@ namespace L6_Visual_Editor
             protected Color border;
             public Color Border { set { border = value; } get { return border; } }
 
-            private string shape;
+            protected int borderSize = 3;
+            public int BorderSize { set { borderSize = value; } get { return borderSize; } }
+
+            protected bool borderChange = false;
+            public bool BorderChange { set { borderChange = value; } get { return borderChange; } }
+
+            protected string shape;
             public string Shape { set { shape = value; } get { return shape; } }
         }
 
@@ -209,13 +250,21 @@ namespace L6_Visual_Editor
             if (colorDialog1.ShowDialog() == DialogResult.Cancel)
                 return;
             pbColor.BackColor = colorDialog1.Color;
+            storage.change_color(pbColor.BackColor);
         }
 
         private void pbBorder_Click(object sender, EventArgs e)
         {
-            if (colorDialog1.ShowDialog() == DialogResult.Cancel)
+            if (colorDialog2.ShowDialog() == DialogResult.Cancel)
                 return;
-            pbBorder.BackColor = colorDialog1.Color;
+            pbBorder.BackColor = colorDialog2.Color;
+        }
+
+        private void cmbxShape_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbxShape.SelectedItem == cmbxShape.Items[0]) storage.Status = "Square";
+            else if (cmbxShape.SelectedItem == cmbxShape.Items[1]) storage.Status = "Triangle";
+            else if (cmbxShape.SelectedItem == cmbxShape.Items[2]) storage.Status = "Circle";
         }
     }
 }
